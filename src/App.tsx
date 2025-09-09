fd6f363ab94c52132e28e9cfea5595fc15a3eed9
sebwebdeb <contact@sebastianmateus.com>
2025-09-09 17:54:51 -0400

    Add instant scroll reset on route navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { BlogPage } from './pages/BlogPage';
 import { BlogPost } from './components/Blog/BlogPost';
+import { ScrollToTop } from './components/ScrollToTop';
 import "./App.css";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<BlogPage />} />
